383ca7441f96392f4ee8186ce66df6c2d9482009
Gulnaaz <gulnaazparweenpap@gmail.com>
2024-04-19 20:08:13 +0530

    chore : add component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 
 
+import About from "./Components/About"
+import Contact from "./Components/Contact"
 import Dodecadronshape from "./Components/Dodecadronshape"
 import Hero from "./Components/Hero"
 import Project from "./Components/Project"
+import Skills from "./Components/Skills"
 import Twojointshpae from "./Components/Twojointshpae"
+import Work from "./Components/Work"
 
 function App() {
  
@@ -13,6 +17,10 @@ function App() {
       {/* <Twojointshpae/> */}
      {/* <Dodecadronshape/> */}
      <Hero/>
+     <About/>
+     <Skills/>
+     <Work/>
+     <Contact/>
     </>
   )
 }
